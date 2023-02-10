7ebbe99c1eb95a67a0deb25387a4764d31dd53ac
jennelie01 <coje22ad@student.ju.se>
2023-02-10 13:47:35 +0100

    initial rainbow and scenery

--- luma-star.js
+++ luma-star.js
@@ -1,9 +1,28 @@
+// luma star
 function lumaStar(x, y) {
   push();
   translate(x, y);
 
+  // rainbow
+  noStroke();
+  fill(255, 0, 0);
+  rect(211.74, 275.92, 12, 60);
+  fill(255, 200, 0);
+  rect(223.11, 275.92, 12, 60);
+  fill(255, 255, 0);
+  rect(235.11, 275.92, 12, 60);
+  fill(0, 255, 0);
+  rect(246, 275.92, 12, 60);
+  fill(0, 0, 255);
+  rect(258.11, 275.92, 12, 60);
+  fill(102, 45, 145);
+  rect(270.11, 275.92, 12, 60);
+  fill(146, 39, 142);
+  rect(282.11, 275.92, 12, 60);
+
   // star shape
-  strokeWeight(0.5);
+  stroke(0);
+  strokeWeight(1);
   fill(255, 255, 0);
   beginShape();
   vertex(251.89, 148.9);
@@ -28,8 +47,25 @@ function lumaStar(x, y) {
   fill(255, 255, 255);
   ellipse(233.33, 211.42, 5, 10);
   ellipse(268.06, 211.42, 5, 10);
+
+  pop();
+}
+
+// scenery
+function scenery() {
+  background(0, 0, 0);
+  noStroke();
+  fill(107, 107, 107);
+  rect(0, 450, width);
+  fill(65, 65, 65);
+  ellipse(60, 494, 40, 30);
+  ellipse(406, 504, 40, 30);
+  ellipse(526, 536, 40, 30);
+  ellipse(89, 546, 40, 30);
+  ellipse(233, 501, 40, 30);
 }
 
 function draw() {
-  lumaStar(50, 50);
+  scenery();
+  lumaStar(10, -90);
 }
