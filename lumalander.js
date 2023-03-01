1b52b0ccdf8753610567e7c024490a19913b9e8d
jennelie01 <coje22ad@student.ju.se>
2023-03-01 18:14:43 +0100

    index.html file + accessible to desktop

--- lumalander.js
+++ lumalander.js
@@ -1,4 +1,8 @@
-createCanvas(591, 724);
+function setup() {
+  //createCanvas(800, 670);
+  createCanvas(windowWidth, windowHeight);
+  frameRate(50);
+}
 
 // luma star
 function lumaStar(x, y, rotation) {
@@ -62,13 +66,18 @@ function scenery() {
   background(0, 0, 0);
   noStroke();
   fill(107, 107, 107);
-  rect(0, 618, width);
+  rect(0, 560, width);
   fill(65, 65, 65);
-  ellipse(60, 694, 40, 30);
-  ellipse(406, 704, 40, 30);
-  ellipse(526, 700, 40, 30);
-  ellipse(89, 660, 40, 30);
-  ellipse(233, 701, 40, 30);
+  ellipse(130, 628, 40, 30);
+  ellipse(370, 635, 40, 30);
+  ellipse(526, 632, 40, 30);
+  ellipse(89, 630, 40, 30);
+  ellipse(233, 625, 40, 30);
+  ellipse(800, 628, 40, 30);
+  ellipse(700, 618, 40, 30);
+  ellipse(900, 620, 40, 30);
+  ellipse(1000, 615, 40, 30);
+  ellipse(1200, 630, 40, 30);
 }
 
 // the following 20 lines have been adapted from lecture about states
@@ -95,7 +104,7 @@ function mouseClicked() {
 }
 
 // the following 9 lines have been adapted from flappy bird example
-let xPosition = 300;
+let xPosition = 700;
 let yPosition = 100;
 let velocity = 1;
 let acceleration = 0.2;
@@ -108,10 +117,10 @@ let speed = 0;
 function startGame() {
   background(0, 0, 0);
   fill(255, 255, 255);
-  text("Welcome to the Luma Lander game!", 70, 240);
+  text("Welcome to the Luma Lander game!", 470, 240);
   textSize(30);
   fill(255, 255, 0);
-  text("Click on the screen to start", 140, 400);
+  text("Click on the screen to start", 540, 400);
 }
 
 // the following lines about velocity and acceleration have been adapted from lectures
@@ -139,18 +148,18 @@ function playGame() {
   velocity = velocity + acceleration;
 
   // the following 16 lines of code has been adapted from Linus Isaksson
-  if (yPosition >= 600 && velocity >= 5) {
+  if (yPosition >= 560 && velocity >= 5) {
     state = "lost";
     yPosition = 100;
-    xPosition = 300;
+    xPosition = 700;
     velocity = 1;
     acceleration = 0.2;
     speed = 0;
     lostGame();
-  } else if (yPosition >= 600 && velocity < 5) {
+  } else if (yPosition >= 560 && velocity < 5) {
     state = "won";
     yPosition = 100;
-    xPosition = 300;
+    xPosition = 700;
     velocity = 1;
     acceleration = 0.2;
     speed = 0;
@@ -160,14 +169,14 @@ function playGame() {
 
 function lostGame() {
   fill(255, 255, 0);
-  text("You lost!", 235, 240);
+  text("You lost!", 435, 240);
   fill(255, 255, 255);
-  text("Click on the screen to try again", 90, 350);
+  text("Click on the screen to try again", 290, 350);
 }
 
 function wonGame() {
   fill(255, 255, 0);
-  text("You won!", 235, 240);
+  text("You won!", 435, 240);
   fill(255, 255, 255);
-  text("click on the screen to play again", 90, 350);
+  text("click on the screen to play again", 290, 350);
 }
